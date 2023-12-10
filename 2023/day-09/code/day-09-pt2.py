c170905e561ded8c1a483c6996b5db7a44666835
sway <sway@localhost.localdomain>
2023-12-09 21:59:28 -0500

    day 09 solutions

---
+++ 2023/day-09/code/day-09-pt2.py
@@ -0,0 +1,70 @@
+import sys
+
+file = sys.argv[1]
+
+with open(file,"r") as f:
+
+    lines = [ [ int(i) for i in line.strip().split() ] for line in f.readlines() ]
+
+final_analysis_sum = 0
+
+for i in lines:
+
+    history = [ n for n in i ]
+
+    predictions = [history[::-1]]
+    analysis = []
+
+    notZero = True
+
+    beg_idx = 0
+
+    while notZero:
+
+        predictions_step = []
+        analysis_step = []
+
+        for i in range(len(predictions[beg_idx]) - 1):
+
+            if not any(predictions[beg_idx]):
+
+                notZero = False
+
+                predictions.pop()
+
+                break
+
+            diff = predictions[beg_idx][i + 1] - predictions[beg_idx][i] 
+
+            predictions_step.append(diff)
+
+        beg_idx = beg_idx + 1
+
+        if notZero:
+
+            predictions.append(predictions_step)
+
+    prediction_next_value = 0
+
+    while predictions:
+
+        results = predictions.pop()
+
+        x = results[-1]
+
+        prediction_next_value = x + prediction_next_value
+
+    final_analysis_sum = final_analysis_sum + prediction_next_value
+
+print(final_analysis_sum)
+
+
+
+
+
+
+            
+
+
+
+            
